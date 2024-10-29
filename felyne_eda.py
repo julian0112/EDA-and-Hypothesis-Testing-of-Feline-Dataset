b62dfcdeae51af4710c67071b66bfd0a6ed129ba
Julian <julian.lsanchez01@gmail.com>
2024-10-29 12:11:20 -0600

    Exploratory Data Analysis and Hypothesis Testing Feline_dataset.xlsx contains the entirety of the dataset including the Seven Feline Behavior and Personality Traits, all the behavioral and personality questions made to the the owners and four questions about the cat regarding the breedgroup, sex, age and if there are other cats in the household. Output.csv contains the Seven Feline Behavior and Personality Traits and the last four questions mentioned, this twelve features are what the project is going to be focused on. Felyne_eda.py is the EDA and hypothesis testing code done with the dataset, it test if a household with just one or multiple cats affects the human sociability score. Honors Peer-graded Assigment is a PDF document that contains a small analysis of the dataset with key findings, charts and the process for the Significant Test of the hypothesis Hypothesis Conclusion: the result of running the test multiple times is a p-value in the range of ~5.83e-08 and ~5.39e-05, the mean of the total p-value was 1.25e-05, this means that we can reject the null hypothesis and say that having another cat affectas the human sociability score, and comparing the median of each sample in the loop tell us that the human sociability is better when there is just one cat.

---
+++ felyne_eda.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.stats import mannwhitneyu
+from sklearn.impute import SimpleImputer, KNNImputer
+from sklearn.preprocessing import OrdinalEncoder, MinMaxScaler
+
+# Reads the dataset and saves only the features that are going to be worked on
+data = pd.read_excel('Feline_dataset.xlsx', 'Dataset')
+data = data.iloc[:, [1, 2, 3, 4, 5, 144, 145, 146, 147, 148, 149, 150]]
+
+# Rename the first four columns and changes the "unkown" values for numpy nan
+data = data.rename(columns={'BREEDGROUP':'breed_group', 'SEX': 'sex', 'AGE_BEHAVIOUR': 'age', 'OTHER_CATS': 'other_cats'})
+data.replace('unknown', np.nan, inplace=True)
+
+# Impute missing ages (127) with the mean of all existing values, then drops all ages older than 17.72 (6)
+age_imp = SimpleImputer(missing_values=np.nan, strategy='mean')
+data.age = age_imp.fit_transform(data[['age']])
+data = data.drop(data[data.age >= 17.72].index)
+
+# Impute missing other_cats data (995) in a ratio of 80% YES and 20% NO, this to maintain the distribution of the existing data
+data.other_cats = data['other_cats'].fillna(pd.Series(np.random.choice(['YES', 'NO'], p=[0.8, 0.2], size=len(data))))
+data.other_cats = data['other_cats'].fillna('NO')
+data.other_cats = data['other_cats'].map({'YES' : True, 'NO' : False})
+
+# Transforms the problematic behavior column into a range of number using Ordinal Encoder
+behaviors = ['No', 'A_little', 'Some', 'A_lot', np.nan]
+enc = OrdinalEncoder(categories= [behaviors])
+data.problematic_behavior = enc.fit_transform(data[['problematic_behavior']])
+
+# Impute missing problematic_behavior data (995) using KNN imputation and the last seven features
+knn = KNNImputer(n_neighbors=2)
+data.iloc[:, 4:11] = knn.fit_transform(data.iloc[:, 4:11])
+data.problematic_behavior = np.round(data.problematic_behavior)
+
+# Mann Whitney U test to compare the human sociability score of the group of households with just one cat 
+# and a sample of similar size of the group of households with more than one cat
+
+total_p_values= 0
+for i in range(5):
+    no_cat = data[data['other_cats'] == False]['human_sociability']
+    other_cat = data[data['other_cats'] == True]['human_sociability'].sample(700)
+    stat, p_value = mannwhitneyu(other_cat, no_cat, alternative='two-sided')
+    total_p_values += p_value
+    print(f'Estadistico: {stat}')
+    print(f'P-value: {p_value}')
+
+    if (no_cat.median() > other_cat.median()):
+        print(f'The median of the households with just one cat, {no_cat.median()}, is greater than the households with multiple cats, {other_cat.median()}')
+        
+    else:
+        print(f'The median of the households with multiple cats, {other_cat.median()}, is greater than the households with just one cat, {no_cat.median()}')
+        
+print('Mean of P-value:', total_p_values/5)
